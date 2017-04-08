9ece0d75055143e9b50c26bf05942a4ce247edc6
Teddy Katz <teddy.katz@gmail.com>
2017-04-08 02:15:45 -0400

    Chore: remove unnecessary abstractions

--- bin/git-delete-squashed.js
+++ bin/git-delete-squashed.js
@@ -7,15 +7,14 @@ const Promise = require('bluebird');
 const DEFAULT_BRANCH_NAME = 'master';
 
 /**
- * Spawns a process
- * @param {string} command The command to run
- * @param {string[]} args The arguments to pass
+ * Calls `git` with the given arguments from the CWD
+ * @param {string[]} args A list of arguments
  * @param {string} [stdin] The stdin for the process
- * @returns {Promise<string>} A Promise for the stdout of the process
+ * @returns {Promise<string>} The output from `git`
  */
-function spawn (command, args, stdin) {
+function git (args, stdin) {
   return new Promise((resolve, reject) => {
-    const child = childProcess.spawn(command, args);
+    const child = childProcess.spawn('git', ['--no-pager'].concat(args));
 
     if (typeof stdin === 'string') {
       child.stdin.write(stdin);
@@ -29,107 +28,52 @@ function spawn (command, args, stdin) {
     child.stderr.on('data', data => stderr += data);
 
     child.on('close', exitCode => exitCode ? reject(stderr) : resolve(stdout));
-  });
+  }).then(stdoutBuffer => stdoutBuffer.toString().replace(/\n$/, ''));
 }
 
-/**
- * Memoizes the result of calling a function
- * @param {Function} func A function that accepts one argument
- * @returns {Function} A function that memoizes `func`
- */
-function memoize (func) {
-  const memo = new Map();
-
-  return arg => {
-    if (!memo.has(arg)) {
-      memo.set(arg, func(arg));
-    }
-    return memo.get(arg);
-  };
-}
-
-/**
- * Calls `git` with the given arguments from the CWD
- * @param {string[]} args A list of arguments
- * @param {string} [stdin] The stdin for the process
- * @returns {Promise<string>} The output from `git`
- */
-function git (args, stdin) {
-  return spawn('git', ['--no-pager'].concat(args), stdin)
-    .then(stdoutBuffer => stdoutBuffer.toString().replace(/\n$/, ''));
-}
-
-/**
- * Gets a list of branch names in the current repo
- * @returns {Promise<string[]>} A list of local branch names
- */
-function getBranchNames () {
-  return git(['for-each-ref', 'refs/heads/', '--format=%(refname:short)']).then(result => result.split('\n'));
-}
-
-/**
- * Gets the hash of the common ancestor between two branches
- * @param {string} branchA A git branch name
- * @param {string} branchB A git branch name
- * @returns {Promise<string>} A commit hash of the latest common ancestor between the two branches
- */
-function getCommonAncestorHash (branchA, branchB) {
-  return git(['merge-base', branchA, branchB]);
-}
-
-/**
- * Gets the diff between two git refs
- * @param {string} fromRef A git reference representing the base of the diff
- * @param {string} toRef A git reference representing the head of the diff
- * @returns {Promise<string>} A git patch ID of the diff between the two refs
- */
-function getDiffPatchId (fromRef, toRef) {
-  return git(['diff', `${fromRef}...${toRef}`]).then(diff => git(['patch-id'], diff));
-}
-
-/**
- * Gets a list of commits to master since a given commit
- * @param {string} fromRef A git reference. Must be an ancestor of `master`
- * @returns {Promise<string[]>} A list of commit hashes with commits to master since `fromRef`
- */
-const getCommitsToMaster = memoize(fromRef => {
-  return git(['log', '--format=%H', `${fromRef}...${DEFAULT_BRANCH_NAME}`])
-    .then(logOutput => logOutput ? logOutput.split('\n') : []);
-});
-
+const commitCache = new Map();
 /**
  * Gets the patch id of a single commit
  * @param {string} hash The commit hash
  * @returns {Promise<string>} The diff of the commit with the given hash
  */
-const getCommitDiffId = memoize(hash => git(['diff', `${hash}^`, hash]).then(diff => git(['patch-id'], diff)));
+function getCommitDiffId (hash) {
+  if (!commitCache.has(hash)) {
+    commitCache.set(hash, git(['diff', `${hash}^`, hash]).then(diff => git(['patch-id'], diff)));
+  }
+  return commitCache.get(hash);
+}
 
-return getBranchNames().tap(branchNames => {
-  if (branchNames.indexOf(DEFAULT_BRANCH_NAME) === -1) {
-    throw `fatal: no branch named '${DEFAULT_BRANCH_NAME}' found in this repo`;
-  }
-}).filter(branchName => {
-  if (branchName === DEFAULT_BRANCH_NAME) {
-    return false;
-  }
+return git(['for-each-ref', 'refs/heads/', '--format=%(refname:short)'])
+  .then(result => result.split('\n'))
+  .tap(branchNames => {
+    if (branchNames.indexOf(DEFAULT_BRANCH_NAME) === -1) {
+      throw `fatal: no branch named '${DEFAULT_BRANCH_NAME}' found in this repo`;
+    }
+  }).filter(branchName => {
+    if (branchName === DEFAULT_BRANCH_NAME) {
+      return false;
+    }
 
-  // Get the common ancestor with the branch and master
-  return getCommonAncestorHash(DEFAULT_BRANCH_NAME, branchName).then(commonAncestorHash =>
-    // Get the diff between the common ancestor and the branch tip
-    getDiffPatchId(commonAncestorHash, branchName).then(branchPatchId =>
-      // Iterate through all the commits to master since the ancestor
-      getCommitsToMaster(commonAncestorHash)
-        .map(getCommitDiffId)
-        // If the patch for any commit to master since the ancestor is the same as the patch between the ancestor
-        // and the branch tip, the branch can be deleted.
-        .map(commitPatchId => commitPatchId === branchPatchId)
-        .then(results => results.some(Boolean))
-    )
-  );
-}).tap(branchNamesToDelete => {
-  if (branchNamesToDelete.length) {
-    return git(['checkout', DEFAULT_BRANCH_NAME]);
-  }
-}).mapSeries(branchName => git(['branch', '-D', branchName]))
+    // Get the common ancestor with the branch and master
+    return git(['merge-base', DEFAULT_BRANCH_NAME, branchName]).then(commonAncestorHash =>
+      // Get the diff between the common ancestor and the branch tip
+      git(['diff', `${commonAncestorHash}...${branchName}`]).then(diff => git(['patch-id'], diff)).then(branchPatchId =>
+        // Iterate through all the commits to master since the ancestor
+        git(['log', '--format=%H', `${commonAncestorHash}...${DEFAULT_BRANCH_NAME}`])
+          .then(logOutput => logOutput ? logOutput.split('\n') : [])
+          .map(getCommitDiffId)
+          // If the patch for any commit to master since the ancestor is the same as the patch between the ancestor
+          // and the branch tip, the branch can be deleted.
+          .map(commitPatchId => commitPatchId === branchPatchId)
+          .then(results => results.some(Boolean))
+      )
+    );
+  }).tap(branchNamesToDelete => {
+    if (branchNamesToDelete.length) {
+      return git(['checkout', DEFAULT_BRANCH_NAME]);
+    }
+  })
+  .mapSeries(branchName => git(['branch', '-D', branchName]))
   .mapSeries(stdout => console.log(stdout))
   .catch(err => console.error(err.cause || err));
