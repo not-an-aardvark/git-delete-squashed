1e5986b64fdebf1ade62110cea28399f100c5650
Jonathan Ling <colonelpopcorn.jl@gmail.com>
2020-02-12 15:31:28 -0500

    Delete merged and squashed from other branches.
    This defaults to master currently, my change proposes that the script should take an argument and allow squashed branches to be pruned no matter where they were merged.

--- bin/git-delete-squashed.js
+++ bin/git-delete-squashed.js
@@ -5,6 +5,7 @@
 const childProcess = require('child_process');
 const Promise = require('bluebird');
 const DEFAULT_BRANCH_NAME = 'master';
+let selectedBranchName;
 
 /**
  * Calls `git` with the given arguments from the CWD
@@ -25,23 +26,27 @@ function git (args) {
   }).then(stdout => stdout.replace(/\n$/, ''));
 }
 
+if (process.argv.length > 0) {
+  selectedBranchName = process.argv[1];
+}
+
 git(['for-each-ref', 'refs/heads/', '--format=%(refname:short)'])
   .then(branchListOutput => branchListOutput.split('\n'))
   .tap(branchNames => {
-    if (branchNames.indexOf(DEFAULT_BRANCH_NAME) === -1) {
-      throw `fatal: no branch named '${DEFAULT_BRANCH_NAME}' found in this repo`;
+    if (branchNames.indexOf((selectedBranchName || DEFAULT_BRANCH_NAME)) === -1) {
+      throw `fatal: no branch named '${selectedBranchName || DEFAULT_BRANCH_NAME}' found in this repo`;
     }
   }).filter(branchName =>
     // Get the common ancestor with the branch and master
     Promise.join(
-      git(['merge-base', DEFAULT_BRANCH_NAME, branchName]),
+      git(['merge-base', selectedBranchName || DEFAULT_BRANCH_NAME, branchName]),
       git(['rev-parse', `${branchName}^{tree}`]),
       (ancestorHash, treeId) => git(['commit-tree', treeId, '-p', ancestorHash, '-m', `Temp commit for ${branchName}`])
     )
-      .then(danglingCommitId => git(['cherry', DEFAULT_BRANCH_NAME, danglingCommitId]))
+      .then(danglingCommitId => git(['cherry', selectedBranchName || DEFAULT_BRANCH_NAME, danglingCommitId]))
       .then(output => output.startsWith('-'))
   )
-  .tap(branchNamesToDelete => branchNamesToDelete.length && git(['checkout', DEFAULT_BRANCH_NAME]))
+  .tap(branchNamesToDelete => branchNamesToDelete.length && git(['checkout', selectedBranchName || DEFAULT_BRANCH_NAME]))
   .mapSeries(branchName => git(['branch', '-D', branchName]))
   .mapSeries(stdout => console.log(stdout))
   .catch(err => console.error(err.cause || err));
